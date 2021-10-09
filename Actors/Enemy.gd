d2e23940697864250402a8113763fb4d2d18f2c5
Necco <eudoaraujo@alu.ufc.br>
2021-10-09 00:47:57 -0300

    Animações inimigo; ToDo: State machine inimigo

---
+++ Actors/Enemy.gd
@@ -0,0 +1,34 @@
+# tool, class_name and extends keywords
+extends KinematicBody2D
+# description (docstring)
+
+# signals
+
+# enums
+enum States {IDLE, RUN, ATTACK, HIT}
+
+# constants
+
+# exported variables
+
+# public variables
+
+# private variables
+
+# onready variables
+
+
+# built-in methods (_init, _ready and others)
+func _ready():
+	pass
+
+
+# public methods
+
+
+# private methods
+
+
+
+
+
