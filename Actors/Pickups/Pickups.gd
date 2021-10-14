19aaf281df3ef1a1204c13b2c1990037310a2883
Necco <eudoaraujo@alu.ufc.br>
2021-10-14 16:53:23 -0300

    FIX: Pickups estavam registrando 2x

--- Actors/Pickups/Pickups.gd
+++ Actors/Pickups/Pickups.gd
@@ -1,13 +1,19 @@
 # tool, class_name and extends keywords
 extends Area2D
 
+# public variables
+var collected := false
+
 # onready variables
 onready var anim_player := $AnimationPlayer
 
 
 # private methods
 func send_pickup(body: Node) -> void:
-	set_deferred("monitorable", false)
+	if collected:
+		return
+
+	collected = true
 	body.get_fruit()
 	anim_player.play("collected")
 	yield(anim_player, "animation_finished")
